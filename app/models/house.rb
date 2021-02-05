159cd7843ca79424d6a968be1619484a332d78fe
HumanBojack <spychalar59@gmail.com>
2021-02-05 15:52:30 +0100

    No overlaps thing and seed (new Gemfile)

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,6 +5,6 @@ class House < ApplicationRecord
 	validates :welcome_message, presence: true
 	belongs_to :admin, class_name: "User"
 	belongs_to :city
-	has_many :reservations
+	has_many :reservations, dependent: :destroy
 end
 # h = House.create(avaliable_beds: 3, price: 13, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat ante sit amet mattis egestas. Quisque varius ornare efficitur vel.", has_wifi: true, welcome_message: "Salut", city: c, user: u)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.all.destroy_all
+User.all.destroy_all
+
+20.times do
+	User.create(email: Faker::Internet.email, phone: "06#{rand(00000000..99999999)}" , description: Faker::Lorem.characters(number: 50))
+end
+10.times do
+	City.create(name: Faker::Address.city, zip_code: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/.random_example)
+end
+50.times do 
+	h = House.create(avaliable_beds: rand(1..8), price: rand(1..95), description: Faker::Lorem.characters(number: 140), welcome_message: Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 4), admin: User.all.sample, city: City.all.sample, has_wifi: [true, false].sample)
+	5.times do 
+		date = Faker::Date.between(from: 2.years.ago, to: Date.today)
+		Reservation.create(start_date: date, end_date:date + rand(1..12).days, house: h, guest: User.all.sample)
+	end
+	5.times do 
+		date = Faker::Date.between(from: Date.today, to: 2.years.from_now)
+		Reservation.create(start_date: date, end_date:date + rand(1..12).days, house: h, guest: User.all.sample)
+	end
+end
