c3c969b9f1cf1748a6d84acec0faea8fe0fc37ed
HumanBojack <spychalar59@gmail.com>
2021-02-05 13:53:38 +0100

    Cleaning and adding presence to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,12 +2,14 @@ class Reservation < ApplicationRecord
 	validates :start_date, presence: true
 	validates :end_date, presence: true
 	validate :end_before_start
+	validates :guest, presence: true
+	validates :house, presence: true
 	def end_before_start
 		errors.add(:start_date, "must be before end time") unless start_date < end_date
 	end
-	belongs_to :guest, class_name: "User"
-	belongs_to :house
 	def duration
 		(end_date - start_date)/60/60/24
 	end
+	belongs_to :guest, class_name: "User"
+	belongs_to :house
 end
